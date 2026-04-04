aabb24637929151444be50f5746e2a3c2ef18d21
AndrewVFranco <129307231+AndrewVFranco@users.noreply.github.com>
2026-04-04 02:58:33 -0700

    Add XML parsing helper function
    * Add pubmed data retrieval function
    
    * Add xml parsing helper function
    
    * Fix path to key in config

--- src/retrieval/pubmed.py
+++ src/retrieval/pubmed.py
@@ -1,8 +1,28 @@
 import requests
+from lxml import etree
 from src.core.config import settings
+
+def parse_data(xml_text: str) -> list[dict]:
+    article_list = []
+    root = etree.fromstring(xml_text.encode("utf-8"))
+
+    articles = root.findall(".//PubmedArticle")
+
+    # For each article, extract fields using findtext
+    for article in articles:
+        pmid = article.findtext(".//PMID")
+        title = article.findtext(".//ArticleTitle")
+        abstract = article.findtext(".//AbstractText")
+        article_data = {"pmid": pmid, "title": title, "abstract": abstract}
+        if article_data["abstract"] is not None:
+            article_list.append(article_data)
+
+    return article_list
+
 
 def search_pubmed(query: str, max_results: int = 10) -> list[dict]:
     base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
+    article_list = []
 
     esearch_params = {
         "db": "pubmed",
@@ -29,7 +49,10 @@ def search_pubmed(query: str, max_results: int = 10) -> list[dict]:
         raw_data = requests.get(f"{base_url}efetch.fcgi", params=efetch_params)
         raw_data.raise_for_status()
 
-        return raw_data.text
+        article_list = parse_data(raw_data.text)
+
+        return article_list
     except Exception as e:
         print(f"Error: {e}")
+
 
