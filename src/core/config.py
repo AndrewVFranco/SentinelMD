b35f0cf0f94f4e1e6b7a93dd36f44eff6f99cd90
AndrewVFranco <129307231+AndrewVFranco@users.noreply.github.com>
2026-04-08 05:56:18 -0700

    Update project to use Gemini API

--- src/core/config.py
+++ src/core/config.py
@@ -8,12 +8,8 @@ class Settings(BaseSettings):
     # Hugging Face
     HF_TOKEN: str
 
-    # Ollama
-    OLLAMA_SERVER_IP: str
-    OLLAMA_SERVER_PORT: int = 11434
-    OLLAMA_MODEL: str = "llama3.2"
-    OLLAMA_MAX_TOKENS: int = 1024
-    OLLAMA_TEMPERATURE: float = 0.1
+    # Gemini
+    GEMINI_API_KEY: str
 
     # Pinecone
     PINECONE_API_KEY: str
