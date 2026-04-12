4f5f1943b2a7f92d38b74076919af0c3d767596e
AndrewVFranco <129307231+AndrewVFranco@users.noreply.github.com>
2026-04-12 13:03:08 -0700

    Remove unused import

--- src/api/main.py
+++ src/api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, HTTPException
 from contextlib import asynccontextmanager
 from src.agent.graph import agent
 from pydantic import BaseModel
